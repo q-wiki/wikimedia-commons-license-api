1519064138be57982e9d680382fbb60c940f89ea
heyarne <arne@schlueter.is>
2020-01-02 18:50:38 +0100

    Add tests for CC-BY 2.0, CC-BY-SA 4.0 and some API conveniences

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const {
   PORT = 3000
 } = process.env
 
+const { URL } = require('url')
 const polka = require('polka') // http server
 const axios = require('axios') // http client
 const cheerio = require('cheerio')
 
 const wikimediaCommonsUrl = /^https?:\/\/commons.wikimedia.org/
+
 
 /**
  * Given a wikimedia commons URL, returns an object describing the license in
@@ -23,19 +25,37 @@ async function parseLicense (url) {
   const res = await axios.get(url)
   const $ = cheerio.load(res.data)
 
-  // the information about which ID contains what is taken from
-  // https://commons.wikimedia.org/wiki/Commons:Machine-readable_data#Machine_readable_data_set_by_infobox_templates
-  const authorNode = $('#fileinfotpl_aut + td a')
-  const author = {
-    name: authorNode.text(),
-    href: authorNode.attr('href')
+  const absoluteUrl = href => href == null ? null : (new URL(href, url)).href
+
+  // Remove empty strings to make it clear that the emptiness is intentional
+  const removeEmpty = str => str === '' ? null : str
+
+  // Helper to extract information from a wikimedia commons page; the data is
+  // identified with different classes and IDs which we can use to traverse the
+  // DOM and extract it. Sometimes the data is given as simple text, sometimes
+  // it's a link where the target URL is also of interest
+  const extractFromNode = node => {
+    const link = node.find('a')
+    if (link.length) {
+      return {
+        label: removeEmpty(link.text().trim()),
+        href: removeEmpty(absoluteUrl(link.attr('href')))
+      }
+    } else {
+      return {
+        href: null,
+        label: node.text().trim()
+      }
+    }
   }
 
-  const sourceNode = $('#fileinfotpl_src + td a')
-  const source = {
-    name: sourceNode.text(),
-    href: sourceNode.attr('href')
-  }
+  // the information about which ID contains what is taken from
+  // https://commons.wikimedia.org/wiki/Commons:Machine-readable_data#Machine_readable_data_set_by_infobox_templates
+  const authorNode = $('#fileinfotpl_aut + td')
+  const author = extractFromNode(authorNode)
+
+  const sourceNode = $('#fileinfotpl_src + td')
+  const source = extractFromNode(sourceNode)
 
   const date = $('#fileinfotpl_date + td time').attr('datetime')
 
@@ -45,9 +65,11 @@ async function parseLicense (url) {
   if (permNode.length) {
     throw new Error('Permissions via #fileinfotpl_perm not implemented!')
   } else {
+    const englishLicense = href => href.replace(/\/deed.(\w+)$/, '') + '/deed.en'
+
     license = {
-      href: $('.licensetpl_link').text().trim(),
-      name: $('.licensetpl_short').text().trim()
+      href: englishLicense($('.licensetpl_link').text().trim()),
+      label: $('.licensetpl_short').text().trim()
     }
   }
 
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('ava')
 const parseLicense = require('./index.js').parseLicense
 
-test('Should throw for non-commons-URLs', async t => {
+test('Throws an error for non-commons-URLs', async t => {
   try {
     await parseLicense('https://eu.dl.twrp.me/beryllium/twrp-3.3.0-0-beryllium.img.html')
     t.fail()
@@ -10,21 +10,72 @@ test('Should throw for non-commons-URLs', async t => {
   }
 })
 
-test('Parsing CC-BY-SA 2.5', async t => {
+test('Parses CC-BY 2.0', async t => {
+  const license = await parseLicense('https://commons.wikimedia.org/wiki/File:Night_in_Shengping_Theater_entry_20120407a.jpg')
+  t.deepEqual({
+    date: '2012-04-07 18:38:43',
+    author: {
+      href: null,
+      label: 'neverbutterfly'
+    },
+    source: {
+      href: 'https://www.flickr.com/photos/7451276@N08/7144848799',
+      label: 'https://www.flickr.com/photos/7451276@N08/7144848799'
+    },
+    license: {
+      href: 'https://creativecommons.org/licenses/by/2.0/deed.en',
+      label: 'CC BY 2.0'
+    }
+  }, license)
+})
+
+test('Parses author without link', async t => {
+  const license = await parseLicense('https://commons.wikimedia.org/wiki/File:Night_in_Shengping_Theater_entry_20120407a.jpg')
+  t.deepEqual({
+    href: null,
+    label: 'neverbutterfly'
+  }, license.author)
+})
+
+test('Parses CC-BY-SA 2.5', async t => {
   const license = await parseLicense('https://commons.wikimedia.org/wiki/File:Lak%C3%B3h%C3%A1z_(18303._sz%C3%A1m%C3%BA_m%C5%B1eml%C3%A9k).jpg')
   t.deepEqual({
     date: '2011-09-30',
     author: {
-      name: 'ara501',
+      label: 'ara501',
       href: 'http://indafoto.hu/ara501'
     },
     source: {
-      name: 'Indafotó',
+      label: 'Indafotó',
       href: 'http://indafoto.hu/ara501/image/13323665-eac38839'
     },
     license: {
-      name: 'CC BY-SA 2.5 hu',
+      label: 'CC BY-SA 2.5 hu',
       href: 'https://creativecommons.org/licenses/by-sa/2.5/hu/deed.en'
     }
   }, license)
+})
+
+test('Parses CC-BY-SA 4.0', async t => {
+  const license = await parseLicense('https://commons.wikimedia.org/wiki/File:%D0%A7%D0%B0%D1%81%D0%BE%D0%B2%D0%BD%D1%8F_%D0%BD%D0%B0%D0%B4_%D0%BC%D0%BE%D0%B3%D0%B8%D0%BB%D0%BE%D0%B9_%D0%BF%D0%B5%D0%B2%D0%B8%D1%86%D1%8B_%D0%90%D0%BD%D0%B0%D1%81%D1%82%D0%B0%D1%81%D0%B8%D0%B8_%D0%92%D1%8F%D0%BB%D1%8C%D1%86%D0%B5%D0%B2%D0%BE%D0%B9.jpg')
+  t.deepEqual({
+    date: '2019-07-10 18:52:18',
+    author: {
+      href: 'https://commons.wikimedia.org/w/index.php?title=User:Anastasia_Galyamicheva&action=edit&redlink=1',
+      label: 'Anastasia Galyamicheva'
+    },
+    source: {
+      href: null,
+      label: 'Own work'
+    },
+    license: {
+      label: 'CC BY-SA 4.0',
+      href: 'https://creativecommons.org/licenses/by-sa/4.0/deed.en'
+    }
+  }, license)
+})
+
+test('Converts relative to absolute URLs', async t => {
+  const license = await parseLicense('https://commons.wikimedia.org/wiki/File:%D0%A7%D0%B0%D1%81%D0%BE%D0%B2%D0%BD%D1%8F_%D0%BD%D0%B0%D0%B4_%D0%BC%D0%BE%D0%B3%D0%B8%D0%BB%D0%BE%D0%B9_%D0%BF%D0%B5%D0%B2%D0%B8%D1%86%D1%8B_%D0%90%D0%BD%D0%B0%D1%81%D1%82%D0%B0%D1%81%D0%B8%D0%B8_%D0%92%D1%8F%D0%BB%D1%8C%D1%86%D0%B5%D0%B2%D0%BE%D0%B9.jpg')
+  t.true(/^https?:\/\//.test(license.author.href))
 })
